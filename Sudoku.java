b01c29361a6d6dd4aaafd037572ca0542de01210
Rohit Kumar <44257572+Rajgeel@users.noreply.github.com>
2021-02-15 16:47:12 +0530

    Add files via upload

---
+++ Sudoku.java
@@ -0,0 +1,114 @@
+// Sudoku is a puzzle where you're given a partially-filled 9 by 9 grid with digits. 
+// The objective is to fill the grid with the constraint that every row, column, and box (3 by 3 subgrid) must contain all of the digits from 1 to 9.
+
+// Implement an efficient sudoku solver.
+
+
+
+//Time complexity is O(9^(n*n)) and space complexity is O(n*n).
+class Sudoku {
+	
+    int N = 9;
+ 
+    boolean solveSuduko(int grid[][], int row, int col) {
+ 
+        if(row == N - 1 && col == N){
+
+            return true;
+        }
+
+        if (col == N) {
+
+            row++;
+            col = 0;
+        }
+
+        if (grid[row][col] != 0){
+
+            return solveSuduko(grid, row, col + 1);
+        }
+ 
+        for(int num = 1; num < 10; num++) {
+
+            if (isSafe(grid, row, col, num)) {
+
+                grid[row][col] = num;
+ 
+                if (solveSuduko(grid, row, col + 1)) {
+
+                    return true;
+                }
+            }
+
+            grid[row][col] = 0;
+        }
+
+        return false;
+    }
+ 
+  	void print(int[][] grid) {
+	    for (int i = 0; i < N; i++) {
+	        for (int j = 0; j < N; j++)
+	            System.out.print(grid[i][j] + " ");
+	        System.out.println();
+	    }
+    }
+ 
+    boolean isSafe(int[][] grid, int row, int col, int num) {
+
+        for(int x = 0; x <= 8; x++) {
+
+            if(grid[row][x] == num) {
+
+                return false;
+            }
+        }
+
+        for(int x = 0; x <= 8; x++) {
+
+            if(grid[x][col] == num) {
+
+                return false;
+            }
+        }
+
+        int startRow = row - row % 3, startCol
+                                      = col - col % 3;
+        for(int i = 0; i < 3; i++) {
+
+            for(int j = 0; j < 3; j++) {
+
+                if(grid[i + startRow][j + startCol] == num) {
+
+                    return false;
+                }
+            }
+        }
+ 
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+    	Sudoku obj = new Sudoku();
+
+        int grid[][] = { { 3, 0, 6, 5, 0, 8, 4, 0, 0 },
+                         { 5, 2, 0, 0, 0, 0, 0, 0, 0 },
+                         { 0, 8, 7, 0, 0, 0, 0, 3, 1 },
+                         { 0, 0, 3, 0, 1, 0, 0, 8, 0 },
+                         { 9, 0, 0, 8, 6, 3, 0, 0, 5 },
+                         { 0, 5, 0, 0, 9, 0, 6, 0, 0 },
+                         { 1, 3, 0, 0, 0, 0, 2, 5, 0 },
+                         { 0, 0, 0, 0, 0, 0, 0, 7, 4 },
+                         { 0, 0, 5, 2, 0, 6, 3, 0, 0 } };
+ 
+        if (obj.solveSuduko(grid, 0, 0)){
+
+            obj.print(grid);
+        }
+        else{
+
+            System.out.println("No Solution exists");
+        }
+    }
+}
